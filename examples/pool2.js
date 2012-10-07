d8988b87eb323eb389f00ffe17b6cca80fa30204
lauris <lauris@ma-1.lv>
2012-10-07 19:35:04 +0300

    add pool example using async queue

---
+++ examples/pool2.js
@@ -0,0 +1,57 @@
+var request = require('request');
+var tidy = require('../htmltidy').tidy;
+var queue = require('async').queue;
+
+
+function Pool(opts, size) {
+
+  function worker(text, cb) {
+    tidy(text, opts, cb)
+  }
+
+  var q = queue(worker, size);
+
+  this.tidy = function(text, cb) {
+    q.push(text, cb);
+  }
+
+ Object.defineProperty(this, 'length', {
+    get : function() {
+      return q.length();
+    }
+   });
+
+}
+
+// released several requests with small time shift
+// but using only 3 workers simultaneous
+// help keep server resources under control
+
+var QUEUE_SIZE = 3;
+
+var TIDY_OPTS = {
+  indent: true,
+  breakBeforeBr: true,
+  fixUri: true,
+  wrap: 0
+}
+
+var pool = new Pool(TIDY_OPTS, QUEUE_SIZE);
+
+var r = 1;
+var a = 1;
+
+function doRequest() {
+
+console.log('request ' + r + ' started');
+  if (r++ < 10) setTimeout(doRequest, 40);
+
+  request('http://www.delfi.lv', function (error, response, body) {
+    pool.tidy(body, function(err, res) {
+      console.log('request ' +  a++ + ' cleaned');
+      console.log('queue size ' + pool.length);
+    });
+  });
+}
+
+doRequest();
